fca12b826904eb7576495b9d50271c2b48d22e63
Tacio de Souza Campos <taciodesouzacampos@hotmail.com>
2020-12-01 16:46:08 -0300

    add aluno details and querying disciplinas

--- src/SAED.Web/Areas/Aplicador/Controllers/SelecaoController.cs
+++ src/SAED.Web/Areas/Aplicador/Controllers/SelecaoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SAED.Core.Constants;
 using SAED.Infrastructure.Data;
 using SAED.Web.Areas.Aplicador.ViewModels;
@@ -35,15 +36,27 @@ namespace SAED.Web.Areas.Aplicador.Controllers
                 return BadRequest();
             }
 
-            dashboardAplicadorViewModel.Escola = await _context.Escolas.FindAsync(dashboardAplicadorViewModel.EscolaId);
-            dashboardAplicadorViewModel.Etapa = await _context.Etapas.FindAsync(dashboardAplicadorViewModel.EtapaId);
-            dashboardAplicadorViewModel.Turma = await _context.Turmas.FindAsync(dashboardAplicadorViewModel.TurmaId);
-            dashboardAplicadorViewModel.Aluno = await _context.Alunos.FindAsync(dashboardAplicadorViewModel.AlunoId);
+            dashboardAplicadorViewModel.Escola = await _context.Escolas
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == dashboardAplicadorViewModel.EscolaId);
+            
+            dashboardAplicadorViewModel.Etapa = await _context.Etapas
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == dashboardAplicadorViewModel.EtapaId);
+            
+            dashboardAplicadorViewModel.Turma = await _context.Turmas
+                .AsNoTracking().Include(x => x.Turno)
+                .FirstOrDefaultAsync(x => x.Id == dashboardAplicadorViewModel.TurmaId);
+            
+            dashboardAplicadorViewModel.Aluno = await _context.Alunos
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == dashboardAplicadorViewModel.AlunoId);
 
             dashboardAplicadorViewModel.Aluno.Turma = null;
             dashboardAplicadorViewModel.Etapa.Turmas = null;
             dashboardAplicadorViewModel.Turma.Alunos = null;
             dashboardAplicadorViewModel.Turma.Etapa = null;
+            dashboardAplicadorViewModel.Turma.Turno.Turmas = null;
             
             HttpContext.Session.Set("alunoMetadata", dashboardAplicadorViewModel);
             
