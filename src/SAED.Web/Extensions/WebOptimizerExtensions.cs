ba2e0840348c5124a80480360f2a7b63fd5261d2
Tácio de Souza Campos <taciodesouzacampos@hotmail.com>
2021-06-03 08:24:40 -0300

    fix style folder location on server

--- src/SAED.Web/Extensions/WebOptimizerExtensions.cs
+++ src/SAED.Web/Extensions/WebOptimizerExtensions.cs
@@ -10,19 +10,28 @@ namespace SAED.Web.Extensions
     {
         public static IAssetPipeline TryGetRelativeScssFiles(this IAssetPipeline pipeline, out IEnumerable<string> realtiveScssFiles, string webRootPath, string stylesFolderRootLocation)
         {
-                var allFolderFiles = Directory.GetFiles(Path.Join(webRootPath, stylesFolderRootLocation));
-                var absoluteScssFiles = allFolderFiles.Where(file => file.EndsWith(".scss"));
-                var myRealtiveScssFiles = new List<string>();
-                
-                foreach (var scssFile in absoluteScssFiles)
-                {
-                    var scssFileLocation = scssFile.LastIndexOf(stylesFolderRootLocation, StringComparison.Ordinal);
-                    myRealtiveScssFiles.Add(scssFile.Substring(scssFileLocation));
-                }
+            List<string> allFolderFiles;
+            try
+            {
+                allFolderFiles = Directory.GetFiles(Path.Join(webRootPath, stylesFolderRootLocation)).ToList();
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                allFolderFiles = Directory.GetFiles(Path.Join(webRootPath, "src", "SAED.Web", "wwwroot", stylesFolderRootLocation)).ToList();
+            }
+            
+            var absoluteScssFiles = allFolderFiles.Where(file => file.EndsWith(".scss"));
+            var myRealtiveScssFiles = new List<string>();
+            
+            foreach (var scssFile in absoluteScssFiles)
+            {
+                var scssFileLocation = scssFile.LastIndexOf(stylesFolderRootLocation, StringComparison.Ordinal);
+                myRealtiveScssFiles.Add(scssFile.Substring(scssFileLocation));
+            }
 
-                realtiveScssFiles = myRealtiveScssFiles.AsEnumerable();
-                
-                return pipeline;
+            realtiveScssFiles = myRealtiveScssFiles.AsEnumerable();
+            
+            return pipeline;
         }
     }
 }
